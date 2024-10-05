ba596a9457edbfef8d067affb274e79530f614e7
jaewoo.song <xoals19958@gmail.com>
2024-10-05 21:59:15 +0900

    Add: splash screen 적용

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
-import { SafeAreaView, StatusBar, useColorScheme, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+  View,
+  Text,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Header from './app/Header';
 import Icon from 'react-native-vector-icons/Ionicons';
-// HomeScreen과 SettingsScreen 컴포넌트를 임시로 만듭니다.
+import SplashScreen from 'react-native-splash-screen';
+
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Home Screen</Text>
     </View>
   );
@@ -15,28 +22,28 @@ function HomeScreen() {
 
 function RankingScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Ranking Screen</Text>
     </View>
   );
 }
 function AlarmScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Alarm Screen</Text>
     </View>
   );
 }
 function LikeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Like Screen</Text>
     </View>
   );
 }
 function SettingScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Setting Screen</Text>
     </View>
   );
@@ -45,11 +52,17 @@ function SettingScreen() {
 const Tab = createBottomTabNavigator();
 
 function App(): React.JSX.Element {
-  const systemColorScheme = useColorScheme();  // 시스템 기본 테마 감지
+  const systemColorScheme = useColorScheme(); // 시스템 기본 테마 감지
   const [isDarkMode, setIsDarkMode] = useState(systemColorScheme === 'dark');
 
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000); //스플래시 활성화 시간
+  });
+
   const toggleTheme = () => {
-    setIsDarkMode(prev => !prev);  // 테마 전환
+    setIsDarkMode(prev => !prev); // 테마 전환
   };
 
   const backgroundStyle = {
@@ -66,14 +79,15 @@ function App(): React.JSX.Element {
         />
         <Header toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
         {/* Tab.Navigator는 독립적으로 사용해야 하므로 다른 컨테이너로 감싸지 않습니다. */}
-        <Tab.Navigator
-        >
+        <Tab.Navigator>
           <Tab.Screen
             name="Home"
             component={HomeScreen}
             options={{
-              tabBarLabel: () => { return null },
-              tabBarIcon: ({ color, size }) => (
+              tabBarLabel: () => {
+                return null;
+              },
+              tabBarIcon: ({color, size}) => (
                 <Icon name="person-circle-outline" color={color} size={30} />
               ),
               headerShown: false,
@@ -83,8 +97,10 @@ function App(): React.JSX.Element {
             name="Ranking"
             component={RankingScreen}
             options={{
-              tabBarLabel: () => { return null },
-              tabBarIcon: ({ color, size }) => (
+              tabBarLabel: () => {
+                return null;
+              },
+              tabBarIcon: ({color, size}) => (
                 <Icon name="star-outline" color={color} size={size} />
               ),
               headerShown: false,
@@ -94,8 +110,10 @@ function App(): React.JSX.Element {
             name="Alarm"
             component={AlarmScreen}
             options={{
-              tabBarLabel: () => { return null },
-              tabBarIcon: ({ color, size }) => (
+              tabBarLabel: () => {
+                return null;
+              },
+              tabBarIcon: ({color, size}) => (
                 <Icon name="notifications-outline" color={color} size={size} />
               ),
               headerShown: false,
@@ -105,8 +123,10 @@ function App(): React.JSX.Element {
             name="Like"
             component={LikeScreen}
             options={{
-              tabBarLabel: () => { return null },
-              tabBarIcon: ({ color, size }) => (
+              tabBarLabel: () => {
+                return null;
+              },
+              tabBarIcon: ({color, size}) => (
                 <Icon name="heart-outline" color={color} size={size} />
               ),
               headerShown: false,
@@ -116,8 +136,10 @@ function App(): React.JSX.Element {
             name="Setting"
             component={SettingScreen}
             options={{
-              tabBarLabel: () => { return null },
-              tabBarIcon: ({ color, size }) => (
+              tabBarLabel: () => {
+                return null;
+              },
+              tabBarIcon: ({color, size}) => (
                 <Icon name="settings-outline" color={color} size={size} />
               ),
               headerShown: false,
